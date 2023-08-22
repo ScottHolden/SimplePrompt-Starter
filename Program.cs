f864795d5e9ddc790b435953e4e6f8272ec368af
Scott Holden <scott@nullops.io>
2023-08-22 18:50:13 +1000

    Initial upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+using Azure.Identity;
+
+string endpoint = "https://<fillthisin>.openai.azure.com/";
+string deploymentName = "<fillthisin>";
+var credential = new DefaultAzureCredential();
+
+var client = new OpenAIClient(new Uri(endpoint), credential);
+
+var prompt = """
+
+""".Trim();
+
+// Sample emails can be found in email01.txt and email02.txt, or you can cretae your own!
+
+Response<Completions> response = await client.GetCompletionsAsync(deploymentName, prompt);
+
+foreach (Choice choice in response.Value.Choices)
+{
+    // Output is up to you!
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # SimplePrompt-Starter
+
+This is a sample starter template for writing text summary projects.
+
+Code is intentinally missing from this project, feel free to fill it in or use CoPilot.
